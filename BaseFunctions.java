6db2c82f28c6da63f9538676c64222cc8cb321af
Han Chen <hanchen1996@hotmail.com>
2017-03-03 23:46:28 -0500

    base function

--- BaseFunctions.java
+++ BaseFunctions.java
@@ -2,6 +2,26 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class BaseFunctions {
+	public static String moveToPoint(double x, double y, double dx, double dy, double a, double b) {
+		double ax = (a - x) - dx;
+		double ay = (b - y) - dy;
+		return accelerate(Math.atan2(ay, ax) + Math.PI); // convert to 0 to 2pi
+	}
+
+	public static Point getNearestNeighbour(ArrayList<Point> unvisited, double x, double y, double dx, double dy) {
+		double distance = Double.MAX_VALUE;
+		Point p = null;
+		for (Point point : unvisited) {
+			double a = point.x;
+			double b = point.y;
+			double d = (x - a) * (x - a) + (y - b) * (y - b);
+			if (d < distance) {
+				p = point;
+				distance = d;
+			}
+		}
+		return p;
+	}
 
 	public static Status parseStatus(String status) {
 		Status s = new Status();
@@ -30,23 +50,23 @@ public class BaseFunctions {
 		return s;
 	}
 
-	public String accelerate(Double rad) {
+	public static String accelerate(Double rad) {
 		return "ACCELERATE" + rad.toString() + " 1";
 	}
 
-	public String bomb(Double x, Double y) {
+	public static String bomb(Double x, Double y) {
 		return "BOMB " + x.toString() + " " + y.toString();
 	}
 
-	public String scan(Double x, Double y) {
+	public static String scan(Double x, Double y) {
 		return "SCAN " + x.toString() + " " + y.toString();
 	}
 
-	public String ScoreBoard() {
+	public static String ScoreBoard() {
 		return "SCOREBOARD";
 	}
 
-	public ArrayList<ScoreEntry> parseScoreBoard(String str) {
+	public static ArrayList<ScoreEntry> parseScoreBoard(String str) {
 		Scanner scanner = new Scanner(str);
 		scanner.next("SCOREBOARD_OUT");
 		ArrayList<ScoreEntry> entries = new ArrayList<>();
@@ -64,8 +84,42 @@ public class BaseFunctions {
 		return entries;
 
 	}
-	// public static void main(String[] args) {
-	// Scanner scanner = new Scanner(System.in);
-	// System.out.println(scanner.next("MINES"));
-	// }
+
+	public static Conf parseConf(String str) {
+		Conf conf = new Conf();
+		Scanner scanner = new Scanner(str);
+		scanner.next("CONFIGURATIONS_OUT");
+		scanner.next("[^\\s-]+");
+		conf.mapWidth = scanner.nextDouble();
+		scanner.next("[^\\s-]+");
+		conf.mapHeight = scanner.nextDouble();
+		scanner.next("[^\\s-]+");
+		conf.captureRadius = scanner.nextDouble();
+		scanner.next("[^\\s-]+");
+		conf.visionRadius = scanner.nextDouble();
+		scanner.next("[^\\s-]+");
+		conf.friction = scanner.nextDouble();
+		scanner.next("[^\\s-]+");
+		conf.brakeFriction = scanner.nextDouble();
+		scanner.next("[^\\s-]+");
+		conf.bombPlaceRadius = scanner.nextDouble();
+		scanner.next("[^\\s-]+");
+		conf.bombEffectRadius = scanner.nextDouble();
+		scanner.next("[^\\s-]+");
+		conf.bombDelay = scanner.nextDouble();
+		scanner.next("[^\\s-]+");
+		conf.bombPower = scanner.nextDouble();
+		scanner.next("[^\\s-]+");
+		conf.scanRadius = scanner.nextDouble();
+		scanner.next("[^\\s-]+");
+		conf.scanDelay = scanner.nextDouble();
+		return conf;
+	}
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		String string = scanner.nextLine();
+		Conf conf = parseConf(string);
+		System.out.println(conf.scanDelay);
+	}
 }
---
+++ Conf.java
@@ -0,0 +1,15 @@
+
+public class Conf {
+	public double mapWidth;
+	public double mapHeight;
+	public double captureRadius;
+	public double visionRadius;
+	public double friction;
+	public double brakeFriction;
+	public double bombPlaceRadius;
+	public double bombEffectRadius;
+	public double bombDelay;
+	public double bombPower;
+	public double scanRadius;
+	public double scanDelay;
+}
